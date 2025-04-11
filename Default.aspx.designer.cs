da4e435945a0242c530a8b037e37120e88789991
murashi07 <yvanmurashi@gmail.com>
2025-04-11 14:37:46 +0200

    signup

--- Default.aspx.designer.cs
+++ Default.aspx.designer.cs
@@ -22,5 +22,14 @@ namespace E_Commerce_First
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// rptrProducts control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater rptrProducts;
     }
 }
